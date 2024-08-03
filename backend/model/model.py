2865ed859e08594fe7e909932cbe169b269599a0
August R.M <sjsaug@gmail.com>
2024-08-03 14:19:35 -0400

    use new model

--- backend/model/model.py
+++ backend/model/model.py
@@ -6,11 +6,11 @@ from langchain.prompts import ChatPromptTemplate
 app = Flask(__name__)
 CORS(app)
 
-model = 'test1234'
+model = 'codegeex4'
 PROMPT_TEMPLATE = """
 Context: {context}
 You have been given some code, please review it and discuss any potential issues in "topic", an array of 2-3 potential recommendations in "recommendations", the corresponding confidence from 0-1 on each recommendation as an array corresponding to the index of the recommendations array in "percentages" and any example code in "code". Format your response in this format: {{"topic": "Potential issues", "advice": ["Potential advice 1", "Potential advice 2", "Potential advice 3"], "confidence": [1.00, 0.99, 0.81], "code": "Example code"}}
-Example: {{"topic": "There are issues with the syntax", "advice": ["You should use a semicolon at the end of the line", "You should use a colon after the if statement", "You should use a colon after the else statement"], "confidence": [0.99, 0.98, 0.97], "code": "if (x == 1) {{ print('Hello, World!') }} else {{ print('Goodbye, World!') }}"}}
+Example (do not copy the contents directly, use it as a template and use your own logic given Context): {{"topic": "There are issues with the syntax", "advice": ["You should use a semicolon at the end of the line", "You should use a colon after the if statement", "You should use a colon after the else statement"], "confidence": [0.99, 0.98, 0.97], "code": "if (x == 1) {{ print('Hello, World!') }} else {{ print('Goodbye, World!') }}"}}
 """
 
 @app.route('/flask-route')
