7982a728d703f29420116148cc7d10de7a6a1f74
yukui5401 <brookeyang5401@gmail.com>
2024-08-03 22:22:51 -0400

    simplified JSON formatting, lowered response times

--- backend/model/model.py
+++ backend/model/model.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 CORS(app)
 
 model = 'phi3:mini'
+# PROMPT_TEMPLATE = """
+# Context: {context}
+# You have been given some code, please review it and discuss any potential issues in "topic", an array of 2-3 potential recommendations in "recommendations", the corresponding confidence from 0-1 for each recommendation as an array corresponding to the index of the recommendation and any example code in "code". Return your response in this json format: {{"topic": "Potential issues", "advice": ["Potential advice 1", "Potential advice 2", "Potential advice 3"], "confidence": [int, int, int], "code": "Example code"}}
+# """
 PROMPT_TEMPLATE = """
 Context: {context}
-You have been given some code, please review it and discuss any potential issues in "topic", an array of 2-3 potential recommendations in "recommendations", the corresponding confidence from 0-1 for each recommendation as an array corresponding to the index of the recommendation and any example code in "code". Return your response in this json format: {{"topic": "Potential issues", "advice": ["Potential advice 1", "Potential advice 2", "Potential advice 3"], "confidence": [int, int, int], "code": "Example code"}}
+Evaluate the code above. Return your response in 2 parts: advice and example code.
 """
 
 @app.route('/code-input', methods=['POST'])
