146ea49de92dbefe821ba4f47de7bee5717f1165
yukui5401 <brookeyang5401@gmail.com>
2024-08-03 21:35:42 -0400

    Revert "Revert "fixed backend routing""
    This reverts commit e7ec25e5a33c4063b79b113a1f929c3a780421e7.

--- backend/controllers/AIPredictions.js
+++ backend/controllers/AIPredictions.js
@@ -1,52 +1,56 @@
-const mongoose = require('mongoose');
-const UserMLResponseSchema = require('../model/AIPredictModel');
-const axios = require('axios');
-const fs = require('fs');
-const FormData = require('form-data');
+const mongoose = require("mongoose");
+const UserMLResponseSchema = require("../model/AIPredictModel");
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
 
 const Create = async (req, res, next) => {
-    try {
-        const newAIPredictionResponse = new UserMLResponseSchema(req.body);
-        await newAIPredictionResponse.save();
-        res.status(200).json({ message: "AI Prediction Response Created" });
-
-    } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
-    }
-}
+  try {
+    const newAIPredictionResponse = new UserMLResponseSchema(req.body);
+    await newAIPredictionResponse.save();
+    res.status(200).json({ message: "AI Prediction Response Created" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 const Read = async (req, res, next) => {
-    try {
-        const allAIPredictionResponses = await UserMLResponseSchema.find();
-        res.status(200).json(allAIPredictionResponses);
+  try {
+    const allAIPredictionResponses = await UserMLResponseSchema.find();
+    res.status(200).json(allAIPredictionResponses);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-    } catch (error) {
-        res.status(500).json({ message: "Internal Server Error" });
+const GetCodeInputSuggestion = async (req, res, next) => { // endpoint: 4000/llm/generate_cs
+  const form = new FormData();
+
+  const filePath = "/tmp/devducky.mp3";
+
+  form.append("audio", fs.createReadStream(filePath));
+
+  const response = await axios.post("http://localhost:4000/", form, {
+    headers: form.getHeaders(),
+  });
+
+  const answer = await axios.post( // get code response (in JSON) from model
+    "http://localhost:5001/generate-suggestions",
+    {
+      query: "how are you doing",
+      transcribed: "good, how are you",
     }
-}
+  );
+  const formattedAnswer = answer.data;
+  console.log(typeof(formattedAnswer));
+  res.send(formattedAnswer);
 
-const GetCodeInputSuggestion = async (req, res, next) => {
-    const form = new FormData();
-
-    const filePath = "/tmp/devducky.mp3";
-
-    form.append('audio', fs.createReadStream(filePath));
-
-    const response = await axios.post('http://localhost:4000/', form, {
-        headers: form.getHeaders(),
-    });
-
-    res.status(200).json(response.data);
-
-    // Read a file from the local file system and append it to the form
-
-}
-
-
+  // Read a file from the local file system and append it to the form
+};
 
 module.exports = {
-    Create,
-    Read,
-    GetCodeInputSuggestion
-}
+  Create,
+  Read,
+  GetCodeInputSuggestion,
+};
 //UserMLResponseSchema.
